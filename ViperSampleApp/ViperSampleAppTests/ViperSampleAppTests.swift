a0f61b7d73cabebea6da292ff791c2d2fab1b315
takashico <s1190089@gmail.com>
2021-07-31 23:58:58 +0900

    create app project

---
+++ ViperSampleApp/ViperSampleAppTests/ViperSampleAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  ViperSampleAppTests.swift
+//  ViperSampleAppTests
+//
+//  Created by Takahashi Shiko on 2021/07/31.
+//
+
+import XCTest
+@testable import ViperSampleApp
+
+class ViperSampleAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
